e82358fec1d161bc3ad8f542933f8e64338a0695
Alex Wright <alexwright1324@gmail.com>
2024-07-22 22:37:29 +0100

    first commit

--- flake.nix
+++ flake.nix
@@ -1,10 +1,9 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    flake-utils.url = "github:numtide/flake-utils";
   };
   
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs }:
     let
       forAllSystems = function:
         nixpkgs.lib.genAttrs [
