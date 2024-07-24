af09649ac8ebee17d0eb754a339892895ccfaa35
Alex Wright <alexwright1324@gmail.com>
2024-07-24 18:24:27 +0100

    i swear if its a TWO

--- package.nix
+++ package.nix
@@ -10,5 +10,9 @@ rustPlatform.buildRustPackage rec {
     sha256 = "sha256-kTZoBpQH7KbmkKrpgt9Ou9/d35rIi3PA0LOgB2kg1FA=";
   };
 
+  postInstall = ''
+    mv "$out/libnss_shim.so" "$out/libnss_shim.so.2"
+  '';
+
   cargoHash = "sha256-aZRu9MpU8oYeFMMzDWAvor8yAYZkXDe81GMt29ewcqs=";
 }
